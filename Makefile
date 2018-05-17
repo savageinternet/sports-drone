3f31967c8ec8b90509f09b326e9951be9fc36944
Valkyrie Savage <valkyrie.savage@gmail.com>
2018-05-17 12:46:55 -0400

    add zbar to track qr codes, add gitignore, fixify happily da makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 CFLAGS=-Wall -Wextra $(shell pkg-config --cflags opencv)
-LDFLAGS=$(shell pkg-config --libs opencv)
+LDFLAGS=$(shell pkg-config --libs opencv zbar)
 
 all: build/main
 
 build/main:
+	mkdir -p build
+	rm -f build/*
 	g++ $(CFLAGS) $(LDFLAGS) main.cpp -o build/main
+
+clean:
+	rm -r build
--- main.cpp
+++ main.cpp
@@ -1,49 +1,127 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
- 
+#include <zbar.h>
+
 using namespace cv;
 using namespace std;
- 
+using namespace zbar;
+
+typedef struct
+{
+  string type;
+  string data;
+  vector <Point> location;
+} decodedObject;
+
 // Convert to string
 #define SSTR( x ) static_cast< std::ostringstream & >( \
 ( std::ostringstream() << std::dec << x ) ).str()
- 
+
+// Find and decode barcodes and QR codes
+void decode(Mat &im, vector<decodedObject>&decodedObjects)
+{
+
+  // Create zbar scanner
+  ImageScanner scanner;
+
+  // Configure scanner
+  scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+
+  // Convert image to grayscale
+  Mat imGray;
+  cvtColor(im, imGray,CV_BGR2GRAY);
+
+  // Wrap image data in a zbar image
+  Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+
+  // Scan the image for barcodes and QRCodes
+  int n = scanner.scan(image);
+
+  // Print results
+  for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+  {
+    decodedObject obj;
+
+    obj.type = symbol->get_type_name();
+    obj.data = symbol->get_data();
+
+    // Print type and data
+    cout << "Type : " << obj.type << endl;
+    cout << "Data : " << obj.data << endl << endl;
+
+    // Obtain location
+    for(int i = 0; i< symbol->get_location_size(); i++)
+    {
+      obj.location.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i)));
+    }
+
+    decodedObjects.push_back(obj);
+  }
+}
+
+// Display barcode and QR code location
+void display(Mat &im, vector<decodedObject>&decodedObjects)
+{
+  // Loop over all decoded objects
+  for(int i = 0; i < decodedObjects.size(); i++)
+  {
+    vector<Point> points = decodedObjects[i].location;
+    vector<Point> hull;
+
+    // If the points do not form a quad, find convex hull
+    if(points.size() > 4)
+      convexHull(points, hull);
+    else
+      hull = points;
+
+    // Number of points in the convex hull
+    int n = hull.size();
+
+    for(int j = 0; j < n; j++)
+    {
+      line(im, hull[j], hull[ (j+1) % n], Scalar(255,0,0), 3);
+    }
+
+  }
+
+}
+
 int main(int argc, char **argv) {
     Ptr<Tracker> tracker = TrackerKCF::create();
 
     // Read video
-    VideoCapture video("/Users/candu/Downloads/test-video-2-code.mov");
-     
+    VideoCapture video("../video/test-video-2-code.mov");
+
     // Exit if video is not opened
     if (!video.isOpened()) {
         cout << "Could not read video file" << endl;
         return 1;
     }
-     
+
     // Read first frame
     Mat frame;
     bool ok = video.read(frame);
-     
+
     // Select initial bounding box
     Rect2d bbox = selectROI(frame, false);
- 
+
     // Display bounding box.
     rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
     imshow("Tracking", frame);
-     
+
     tracker->init(frame, bbox);
-     
-    while (video.read(frame)) {     
+
+    while (video.read(frame)) {
         // Start timer
         double timer = (double) getTickCount();
-         
+
         // Update the tracking result
         bool ok = tracker->update(frame, bbox);
-         
+
         // Calculate Frames per second (FPS)
         float fps = getTickFrequency() / ((double) getTickCount() - timer);
-         
+
         if (ok) {
             // Tracking success : Draw the tracked object
             rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
@@ -51,16 +129,25 @@ int main(int argc, char **argv) {
             // Tracking failure detected.
             putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
         }
-         
+
+        // Variable for decoded objects
+        vector<decodedObject> decodedObjects;
+
+        // Find and decode barcodes and QR codes
+        decode(frame, decodedObjects);
+
+        // Display location
+        display(frame, decodedObjects);
+
         // Display tracker type on frame
         putText(frame, "KCF Tracker", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
-         
+
         // Display FPS on frame
         putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
- 
+
         // Display frame.
         imshow("Tracking", frame);
-         
+
         // Exit if ESC pressed.
         int k = waitKey(1);
         if (k == 27) {
